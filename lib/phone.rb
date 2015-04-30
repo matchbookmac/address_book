e78c3d25a9448561707235b45fca10fdf926f95a
Kim Dang & Ian MacDonald <student@epicodus.com>
2015-04-30 09:30:11 -0700

    Add all and save methods to Phone class.

--- lib/phone.rb
+++ lib/phone.rb
@@ -1,9 +1,19 @@
 class Phone
   attr_reader(:area_code, :number, :type)
 
+  @@phones = []
+
   define_method(:initialize) do |attributes|
     @area_code = attributes.fetch(:area_code)
     @number = attributes.fetch(:number)
     @type = attributes.fetch(:type)
+  end
+
+  define_method(:save) do
+    @@phones.push(self)
+  end
+
+  define_singleton_method(:all) do
+    @@phones
   end
 end
--- spec/phone_spec.rb
+++ spec/phone_spec.rb
@@ -2,7 +2,6 @@ require('rspec')
 require('phone')
 
 describe(Phone) do
-
   describe('#area_code') do
     it('returns an area code') do
       test_phone_number = Phone.new(:area_code => '503', :number => '555-1234', :type => 'home')
@@ -21,6 +20,20 @@ describe(Phone) do
     it('returns the type of phone number it is') do
       test_phone_number = Phone.new(:area_code => '503', :number => '555-1234', :type => 'home')
       expect(test_phone_number.type()).to(eq('home'))
+    end
+  end
+
+  describe('.all') do
+    it('returns an empty array') do
+      expect(Phone.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves a phone object to the numbers class instance') do
+      test_phone_number = Phone.new(:area_code => '503', :number => '555-1234', :type => 'home')
+      test_phone_number.save()
+      expect(Phone.all()).to(eq([test_phone_number]))
     end
   end
 end
